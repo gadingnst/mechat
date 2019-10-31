64004dba8bb07669d48a8e9ee0e79927f67b384d
Sutan Nasution <sutan.gnst@gmail.com>
2019-10-31 16:15:52 +0700

    added map feature

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import promiseMiddleware from 'redux-promise-middleware'
--- src/Screens/Maps/index.js
+++ src/Screens/Maps/index.js
@@ -1,23 +1,74 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Text, Dimensions } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import Geolocation, { setRNConfiguration } from 'react-native-geolocation-service'
 import Color from '../../Assets/Color'
 import Header from '../../Components/Header'
 
+const ASPECT_RATIO =
+    Dimensions.get('window').width / Dimensions.get('window').height
+
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+
 export default () => {
+    const [region, setRegion] = useState({
+        latitude: -6.117664,
+        longitude: 106.906349,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+    })
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition(
+            position => {
+                setRegion({
+                    ...region,
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                })
+            },
+            error => {
+                console.log(error)
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 15000,
+                maximumAge: 10000
+            }
+        )
+    }, [])
+
     return (
         <>
             <Header title="Maps" backgroundColor={Color.Success} />
+            <View>
+                <Text>{JSON.stringify(region)}</Text>
+            </View>
             <View style={styles.container}>
-                <Text>Hello from Maps</Text>
+                <MapView
+                    style={styles.mapView}
+                    region={region}
+                    showsUserLocation
+                >
+                    <MapView.Marker coordinate={region} />
+                </MapView>
             </View>
         </>
     )
 }
 
 const styles = StyleSheet.create({
+    mapView: {
+        ...StyleSheet.absoluteFillObject
+    },
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        position: 'absolute',
+        top: 100,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
         alignItems: 'center'
     }
 })
