1afc5625378b7eba710e058ba4f3986ba5c0f92a
Sutan Nasution <sutan.gnst@gmail.com>
2019-11-01 15:19:51 +0700

    setup onesignal

--- src/Config/FirebaseSDK.js
+++ src/Config/FirebaseSDK.js
@@ -1,16 +1,26 @@
 import Firebase from 'firebase'
+import {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_SENDER_ID,
+    FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID
+} from 'react-native-dotenv'
 
 const FirebaseSDK = () => {
     if (!Firebase.apps.length) {
         return Firebase.initializeApp({
-            apiKey: 'AIzaSyCcPnWn7PkPx4WyVMX7Rj9MC6P_bE9K_1U',
-            authDomain: 'mechat-f593b.firebaseapp.com',
-            databaseURL: 'https://mechat-f593b.firebaseio.com',
-            projectId: 'mechat-f593b',
-            storageBucket: 'mechat-f593b.appspot.com',
-            messagingSenderId: '320063284128',
-            appId: '1:320063284128:web:57f75839ad8102235a7ac5',
-            measurementId: 'G-5TS9CQ0ZVE'
+            apiKey: FIREBASE_API_KEY,
+            authDomain: FIREBASE_AUTH_DOMAIN,
+            databaseURL: FIREBASE_DATABASE_URL,
+            projectId: FIREBASE_PROJECT_ID,
+            storageBucket: FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: FIREBASE_SENDER_ID,
+            appId: FIREBASE_APP_ID,
+            measurementId: FIREBASE_MEASUREMENT_ID
         })
     }
     return Firebase.app()
